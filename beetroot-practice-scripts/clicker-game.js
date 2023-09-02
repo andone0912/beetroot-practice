fbc5a24016f919180154c2e6f3267c020751cfe4
Andone0912 <andonedarius04@gmail.com>
2023-09-03 02:19:15 +0300

    clicker game v2

--- beetroot-practice-scripts/clicker-game.js
+++ beetroot-practice-scripts/clicker-game.js
@@ -6,21 +6,46 @@ function addNumber() {
   if(number < 100) {
     number = number + 1;
     counterNumber.innerHTML = number;
-  } else if(number >= 100 & number <= 400) {
+  } else if(number >= 100 & number <= 300) {
+    if (document.querySelector('.upgrades1').classList.contains('js-upgrade1')) {
     number = number + 3;
     counterNumber.innerHTML = number;
-  } else if(number >= 400 & number <= 1600) {
-    number = number + 7;
-    counterNumber.innerHTML = number;
-  } else if(number >= 1600 & number <= 4800) {
-    number = number + 15;
-    counterNumber.innerHTML = number;
-  } else if(number >= 4800 & number <= 10000) {
-    number = number + 31;
-    counterNumber.innerHTML = number;
+    } else {
+      number = number + 1;
+      counterNumber.innerHTML = number;
+    }
+  } else if(number >= 300 & number <= 900) {
+    if (document.querySelector('.upgrades2').classList.contains('js-upgrade2')) {
+      number = number + 7;
+      counterNumber.innerHTML = number;
+      } else {
+        number = number + 3;
+        counterNumber.innerHTML = number;
+      }
+  } else if(number >= 900 & number <= 2700) {
+    if (document.querySelector('.upgrades3').classList.contains('js-upgrade3')) {
+      number = number + 15;
+      counterNumber.innerHTML = number;
+      } else {
+        number = number + 7;
+        counterNumber.innerHTML = number;
+      }
+  } else if(number >= 2700 & number <= 6750) {
+    if (document.querySelector('.upgrades4').classList.contains('js-upgrade4')) {
+      number = number + 31;
+      counterNumber.innerHTML = number;
+      } else {
+        number = number + 15;
+        counterNumber.innerHTML = number;
+      }
   } else {
-    number = number + 67;
-    counterNumber.innerHTML = number;
+    if (document.querySelector('.upgrades5').classList.contains('js-upgrade5')) {
+      number = number + 67;
+      counterNumber.innerHTML = number;
+      } else {
+        number = number + 31;
+        counterNumber.innerHTML = number;
+      }
   }
 
 }
