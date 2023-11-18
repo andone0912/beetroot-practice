8ea387c7aca385d5df9ebcd91fddede7595b0f10
Andone0912 <andonedarius04@gmail.com>
2023-11-19 01:16:00 +0200

    progress on tic tac toe

--- beetroot-practice-scripts/tic-tac-toe.js
+++ beetroot-practice-scripts/tic-tac-toe.js
@@ -1,14 +1,23 @@
 
+const boxes = document.querySelectorAll('.box')
+let symbol = 'o'
+let arr =[]
 function addButtons(number) {
 const element = document.querySelector(`.icon${number}`)
-if(!element.classList.contains('symbol-on')) {
-element.classList.remove('fa-regular', 'fa-circle'); 
-element.classList.add('fa-solid', 'fa-x');
-element.classList.add('symbol-on');
-} else {
-  element.classList.remove('symbol-on')
-  element.classList.remove('fa-solid', 'fa-x');
-  element.classList.add('fa-regular', 'fa-circle'); 
-}
-
-}
+if(document.getElementById('playButton').classList.contains('game-on')) {
+  if(!element.classList.contains('symbol-on')) {
+    if (symbol === 'o') {
+      element.classList.remove('fa-regular', 'fa-circle'); 
+      element.classList.add('fa-solid', 'fa-x');
+      element.classList.add('symbol-on');
+      symbol = 'x'
+    }else if (symbol === 'x'){
+      element.classList.remove('fa-solid', 'fa-x');
+      element.classList.add('fa-regular', 'fa-circle'); 
+      element.classList.add('symbol-on')
+      symbol = 'o'
+    }
+    arr.push(symbol)
+    console.log(arr)
+  }
+}} 
